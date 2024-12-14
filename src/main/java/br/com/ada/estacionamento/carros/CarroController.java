24a30d5d959988e3a41008ab937df33ac6429798
DevGuiBan <guibandeira290@gmail.com>
2024-12-13 21:01:34 -0300

    First Commit

--- src/main/java/br/com/ada/estacionamento/carros/CarroController.java
+++ src/main/java/br/com/ada/estacionamento/carros/CarroController.java
@@ -1,6 +1,7 @@
 package br.com.ada.estacionamento.carros;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,22 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CarroController {
 
-    public final CarroService service;
+    private final CarroService service;
 
     public CarroController(CarroService service) {
         this.service = service;
     }
 
     @PostMapping
-    public ResponseEntity<Carro> cadastrarCarro(@RequestBody Carro carro){
+    public ResponseEntity<Carro> cadastrarCarro(@RequestBody Carro carro) {
         service.cadastrarCarro(carro);
         return ResponseEntity.created(null).body(carro);
     }
 
     @PostMapping
-    public ResponseEntity<Carro> estacionar(@RequestBody Carro carro){
+    public ResponseEntity<String> estacionar(@RequestBody Carro carro) {
         service.estacionar(carro);
-        return ResponseEntity.ok(carro);
+        return ResponseEntity.ok("Carro estacionado com sucesso.");
     }
 
+    @PostMapping
+    public ResponseEntity<String> retirarCarro(@PathVariable String placa) {
+        service.retirarCarro(placa);
+        return ResponseEntity.ok("Carro retirado com sucesso.");
+    }
 }
--- src/main/java/br/com/ada/estacionamento/carros/CarroService.java
+++ src/main/java/br/com/ada/estacionamento/carros/CarroService.java
@@ -1,7 +1,10 @@
 package br.com.ada.estacionamento.carros;
 
+import br.com.ada.estacionamento.vagas.Vaga;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CarroService {
@@ -9,12 +12,47 @@ public class CarroService {
 
     public CarroRepository carroRepository;
 
-    public void cadastrarCarro(Carro carro){
+    public CarroService(CarroRepository carroRepository) {
+        this.carroRepository = carroRepository;
+    }
+
+    public void cadastrarCarro(Carro carro) {
         carroRepository.save(carro);
     }
 
-    public void estacionar(Carro carro){
+    public void estacionar(Carro carro) {
+        if (carro.getVaga() != null) {
+            throw new IllegalStateException("O carro já está estacionado.");
+        }
+
+        Vaga vaga = carro.getVaga();
+        if (vaga == null || vaga.isOcupada()) {
+            throw new IllegalArgumentException("A vaga está ocupada ou não foi informada.");
+        }
+
+        vaga.setOcupada(true);
+        carro.setVaga(vaga);
         carroRepository.save(carro);
     }
+
+    public void retirarCarro(String placa) {
+        Optional<Carro> carroOptional = carroRepository.findById(placa);
+        if (carroOptional.isEmpty()) {
+            throw new IllegalArgumentException("Carro não encontrado.");
+        }
+
+        Carro carro = carroOptional.get();
+
+        Vaga vaga = carro.getVaga();
+        if (vaga == null) {
+            throw new IllegalStateException("O carro não está estacionado.");
+        }
+
+        vaga.setOcupada(false);
+        carro.setVaga(null);
+        carroRepository.save(carro);
+    }
+
+
 
 }
